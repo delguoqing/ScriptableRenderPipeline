63b9b0d4c36a815bc6ac5900177376382ed828c7
Kink3d <matthewdean3d@gmail.com>
2019-05-23 14:40:26 -0700

    Add initial precision colors implementation

---
+++ com.unity.shadergraph/Editor/Drawing/Colors/PrecisionColors.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UnityEditor.ShaderGraph.Drawing.Colors
+{
+    class PrecisionColors : ColorProviderFromStyleSheet
+    {
+        public override string GetTitle() => "Precision";
+
+        public override bool AllowCustom() => false;
+
+        protected override bool GetClassFromNode(AbstractMaterialNode node, out string ussClass)
+        {
+            ussClass = node.concretePrecision.ToString();
+
+            return !string.IsNullOrEmpty(ussClass);
+        }
+    }
+}
